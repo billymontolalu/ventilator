7e849df1d6df648c7af5d408104d1dc8518f1b4c
Billy Montolalu <billy.montolalu@live.com>
2020-11-23 14:17:01 +0700

    add list ventilator and stats

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:oscilloscope/oscilloscope.dart';
-import 'package:number_stepper/number_stepper.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
+import 'package:ventilator/stats.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,261 +32,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<double> volumeData = List();
-  List<double> flowData = List();
-  List<double> pawData = List();
-  double radians = 0.0;
-  Timer _timer;
-  int _ppeak = 18;
-  int _peep = 10;
-  int _bpm = 10;
-  int _ratio = 2;
-  int _volmax = 800;
-
   @override
   initState() {
     super.initState();
-    _timer = Timer.periodic(Duration(milliseconds: 200), _generateTrace);
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  int _generateData(int min, int max) {
-    Random rnd = new Random();
-    return min + rnd.nextInt(max - min);
-  }
-
-  _generateTrace(Timer t) async {
-    //load data from internet
-    final response = await http.get('http://localhost:3000/');
-    print(response);
-
-    //final _random = new Random();
-    //int next(int min, int max) => min + _random.nextInt(max - min);
-    // generate our  values
-    //var sv = sin((radians * pi));
-    //var cv = cos((radians * pi));
-
-    // Add to the growing dataset
-    setState(() {
-      /*if (traceSine.length > 300) {
-        traceSine.removeAt(0);
-      }*/
-      double dataVolume = _generateData(0, 1000).toDouble();
-      volumeData.add(dataVolume);
-
-      double dataPaw = _generateData(-20, 60).toDouble();
-      pawData.add(dataPaw);
-
-      double dataFlow = _generateData(-100, 100).toDouble();
-      flowData.add(dataFlow);
-    });
-
-    // adjust to recyle the radian value ( as 0 = 2Pi RADS)
-    //radians += 0.05;
-    //if (radians >= 2.0) {
-    //radians = 0.0;
-    //}
-  }
-
-  Widget _space() {
-    return SizedBox(
-      height: 15.0,
-    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
-    Oscilloscope flow = Oscilloscope(
-      padding: 20.0,
-      backgroundColor: Colors.black,
-      traceColor: Colors.green,
-      yAxisMax: 100.0,
-      yAxisMin: -100.0,
-      dataSet: flowData,
-      showYAxis: true,
-    );
-
-    Oscilloscope paw = Oscilloscope(
-      padding: 20.0,
-      backgroundColor: Colors.black,
-      traceColor: Colors.redAccent,
-      yAxisMax: 60.0,
-      yAxisMin: -20.0,
-      dataSet: pawData,
-      showYAxis: true,
-    );
-
-    // Create A Scope Display for Cosine
-    Oscilloscope volume = Oscilloscope(
-      showYAxis: true,
-      padding: 20.0,
-      backgroundColor: Colors.black,
-      traceColor: Colors.yellow,
-      yAxisMax: 1000.0,
-      yAxisMin: 0.0,
-      dataSet: volumeData,
-    );
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Ventilator"),
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                    flex: 1,
-                    child: Stack(children: [
-                      paw,
-                      Container(
-                        padding: EdgeInsets.only(top: 5.0),
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          "PAW",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        alignment: Alignment.topRight,
-                        child: CircleAvatar(
-                          radius: 25.0,
-                          child: Text(
-                            "40",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      )
-                    ])),
-                Expanded(
-                  flex: 1,
-                  child: Stack(children: [
-                    flow,
-                    Container(
-                        padding: EdgeInsets.only(top: 5.0),
-                        alignment: Alignment.topCenter,
-                        child: Text("Flow",
-                            style: TextStyle(color: Colors.white))),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      alignment: Alignment.topRight,
-                      child: CircleAvatar(
-                        radius: 25.0,
-                        child: Text(
-                          "50",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                    )
-                  ]),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Stack(children: [
-                    volume,
-                    Container(
-                        padding: EdgeInsets.only(top: 5.0),
-                        alignment: Alignment.topCenter,
-                        child: Text("Volume",
-                            style: TextStyle(color: Colors.white))),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      alignment: Alignment.topRight,
-                      child: CircleAvatar(
-                        radius: 25.0,
-                        child: Text(
-                          "500",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                    )
-                  ]),
-                ),
-              ],
+        appBar: AppBar(
+          title: Text("Ventilator Command Center"),
+        ),
+        body: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 4,
+          childAspectRatio: 16 / 4,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(
+                Icons.people,
+                color: Colors.blue,
+              ),
+              title: Text("Billy Montolalu"),
+              subtitle: Text("Kamar Mawar Bed 1"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Stats()),
+                );
+              },
             ),
-          ),
-          Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Ppeak"),
-                  NumberStepper(
-                    count: _ppeak,
-                    size: 30,
-                    //activeForegroundColor: Colors.purple,
-                    activeBackgroundColor: Colors.red,
-                    didChangeCount: (count) {
-                      setState(() {
-                        _ppeak = count;
-                      });
-                    },
-                  ),
-                  _space(),
-                  Text("PEEP"),
-                  NumberStepper(
-                    count: _peep,
-                    size: 30,
-                    //activeForegroundColor: Colors.purple,
-                    activeBackgroundColor: Colors.red,
-                    didChangeCount: (count) {
-                      setState(() {
-                        _peep = count;
-                      });
-                    },
-                  ),
-                  _space(),
-                  Text("BPM"),
-                  NumberStepper(
-                    count: _bpm,
-                    size: 30,
-                    //activeForegroundColor: Colors.purple,
-                    activeBackgroundColor: Colors.red,
-                    didChangeCount: (count) {
-                      setState(() {
-                        _bpm = count;
-                      });
-                    },
-                  ),
-                  _space(),
-                  Text("I/E Ratio 1"),
-                  NumberStepper(
-                    count: _ratio,
-                    size: 30,
-                    //activeForegroundColor: Colors.purple,
-                    activeBackgroundColor: Colors.red,
-                    didChangeCount: (count) {
-                      setState(() {
-                        _ratio = count;
-                      });
-                    },
-                  ),
-                  _space(),
-                  Text("Maxvolume"),
-                  NumberStepper(
-                    count: _volmax,
-                    size: 30,
-                    //activeForegroundColor: Colors.purple,
-                    activeBackgroundColor: Colors.red,
-                    didChangeCount: (count) {
-                      setState(() {
-                        _volmax = count;
-                      });
-                    },
-                  ),
-                ],
-              ))
-        ],
-      ),
-    );
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 2"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 3"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 4"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 5"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 6"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 7"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 8"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 9"),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Kosong"),
+              subtitle: Text("Kamar Mawar Bed 10"),
+            ),
+          ],
+        ) /*RaisedButton(onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Stats()),
+          );
+        })*/
+        );
   }
 }
---
+++ lib/stats.dart
@@ -0,0 +1,374 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:number_stepper/number_stepper.dart';
+import 'package:oscilloscope/oscilloscope.dart';
+
+import 'constanta.dart';
+
+class Stats extends StatefulWidget {
+  @override
+  _StatsState createState() => _StatsState();
+}
+
+class _StatsState extends State<Stats> {
+  List<double> volumeData = List();
+  List<double> flowData = List();
+  List<double> pawData = List();
+  double radians = 0.0;
+  Timer _timer;
+  int _ppeak = 18;
+  int _peep = 10;
+  int _bpm = 10;
+  int _ratio = 2;
+  int _volmax = 800;
+  double dataPaw = 0;
+  double dataFlow = 0;
+  double dataVolume = 0;
+  String start = "Start";
+  String fio = "0", cp = "0";
+
+  @override
+  initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    _timer = Timer.periodic(Duration(milliseconds: 200), _generateTrace);
+  }
+
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  _generateTrace(Timer t) async {
+    //load data from internet
+    final response = await http.get(BASE_URL);
+    var data = response.body;
+    var split = data.split("#");
+    var paw = split[0];
+    if (paw != "") {
+      dataPaw = double.parse(paw);
+    }
+
+    var volume = split[2];
+    if (volume != "") {
+      dataVolume = double.parse(volume);
+    }
+
+    var flow = split[1];
+    if (flow != "") {
+      dataFlow = double.parse(flow);
+    }
+
+    fio = split[3];
+    cp = split[4];
+
+    setState(() {
+      volumeData.add(dataVolume);
+      pawData.add(dataPaw);
+      flowData.add(dataFlow);
+    });
+  }
+
+  Widget _space() {
+    return SizedBox(
+      height: 15.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Oscilloscope flow = Oscilloscope(
+      padding: 20.0,
+      backgroundColor: Colors.black,
+      traceColor: Colors.green,
+      yAxisMax: 100.0,
+      yAxisMin: -100.0,
+      dataSet: flowData,
+      showYAxis: true,
+    );
+
+    Oscilloscope paw = Oscilloscope(
+      padding: 20.0,
+      backgroundColor: Colors.black,
+      traceColor: Colors.redAccent,
+      yAxisMax: 60.0,
+      yAxisMin: -20.0,
+      dataSet: pawData,
+      showYAxis: true,
+    );
+
+    // Create A Scope Display for Cosine
+    Oscilloscope volume = Oscilloscope(
+      showYAxis: true,
+      padding: 20.0,
+      backgroundColor: Colors.black,
+      traceColor: Colors.yellow,
+      yAxisMax: 1000.0,
+      yAxisMin: 0.0,
+      dataSet: volumeData,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ventilator Monitoring"),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Stack(children: [
+                      paw,
+                      Container(
+                        padding: EdgeInsets.only(top: 5.0),
+                        alignment: Alignment.topCenter,
+                        child: Text(
+                          "PAW",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        alignment: Alignment.topRight,
+                        child: CircleAvatar(
+                          radius: 40.0,
+                          child: Text(
+                            dataPaw.toString(),
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      )
+                    ])),
+                Expanded(
+                  flex: 1,
+                  child: Stack(children: [
+                    flow,
+                    Container(
+                        padding: EdgeInsets.only(top: 5.0),
+                        alignment: Alignment.topCenter,
+                        child: Text("Flow",
+                            style: TextStyle(color: Colors.white))),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.topRight,
+                      child: CircleAvatar(
+                        radius: 40.0,
+                        child: Text(
+                          dataFlow.toString(),
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    )
+                  ]),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Stack(children: [
+                    volume,
+                    Container(
+                        padding: EdgeInsets.only(top: 5.0),
+                        alignment: Alignment.topCenter,
+                        child: Text("Volume",
+                            style: TextStyle(color: Colors.white))),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.topRight,
+                      child: CircleAvatar(
+                        radius: 40.0,
+                        child: Text(
+                          dataVolume.toString(),
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    )
+                  ]),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Image.asset('assets/logo.png', scale: 10.0),
+                      ],
+                    ),
+                  ),
+                  _space(),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Text("FiO2"),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            alignment: Alignment.topRight,
+                            child: CircleAvatar(
+                              radius: 25.0,
+                              child: Text(
+                                fio,
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("CP"),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            alignment: Alignment.topRight,
+                            child: CircleAvatar(
+                              radius: 25.0,
+                              child: Text(
+                                cp,
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text("CP"),
+                  Text("Ppeak"),
+                  NumberStepper(
+                    count: _ppeak,
+                    size: 30,
+                    //activeForegroundColor: Colors.purple,
+                    activeBackgroundColor: Colors.red,
+                    didChangeCount: (count) async {
+                      await http.get(BASE_URL +
+                          "?setting=ppeak&nilai=" +
+                          count.toString());
+                      setState(() {
+                        _ppeak = count;
+                      });
+                    },
+                  ),
+                  _space(),
+                  Text("PEEP"),
+                  NumberStepper(
+                    count: _peep,
+                    size: 30,
+                    //activeForegroundColor: Colors.purple,
+                    activeBackgroundColor: Colors.red,
+                    didChangeCount: (count) async {
+                      await http.get(
+                          BASE_URL + "?setting=peep&nilai=" + count.toString());
+                      setState(() {
+                        _peep = count;
+                      });
+                    },
+                  ),
+                  _space(),
+                  Text("BPM"),
+                  NumberStepper(
+                    count: _bpm,
+                    size: 30,
+                    //activeForegroundColor: Colors.purple,
+                    activeBackgroundColor: Colors.red,
+                    didChangeCount: (count) async {
+                      await http.get(
+                          BASE_URL + "?setting=bpm&nilai=" + count.toString());
+                      setState(() {
+                        _bpm = count;
+                      });
+                    },
+                  ),
+                  _space(),
+                  Text("I/E Ratio 1"),
+                  NumberStepper(
+                    count: _ratio,
+                    size: 30,
+                    //activeForegroundColor: Colors.purple,
+                    activeBackgroundColor: Colors.red,
+                    didChangeCount: (count) async {
+                      await http.get(
+                          BASE_URL + "?setting=er&nilai=" + count.toString());
+                      setState(() {
+                        _ratio = count;
+                      });
+                    },
+                  ),
+                  _space(),
+                  Text("Maxvolume"),
+                  NumberStepper(
+                    count: _volmax,
+                    size: 30,
+                    //activeForegroundColor: Colors.purple,
+                    activeBackgroundColor: Colors.red,
+                    didChangeCount: (count) async {
+                      await http.get(BASE_URL +
+                          "?setting=volmax&nilai=" +
+                          count.toString());
+                      setState(() {
+                        _volmax = count;
+                      });
+                    },
+                  ),
+                  _space(),
+                  FlatButton(
+                    onPressed: () async {
+                      int stateStart;
+                      setState(() {
+                        if (start == "Start") {
+                          start = "Stop";
+                          stateStart = 1;
+                        } else {
+                          start = "Start";
+                          stateStart = 0;
+                        }
+                      });
+                      await http.get(BASE_URL +
+                          "?setting=start&nilai=" +
+                          stateStart.toString());
+                    },
+                    color: Colors.red,
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      // Replace with a Row for horizontal icon + text
+                      children: <Widget>[
+                        Icon(
+                          Icons.replay_outlined,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          start,
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
